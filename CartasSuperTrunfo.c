cde6e199cb8c2852a95acf98752774c4ee1686a2
LuizPFS <luizpfs09@gmail.com>
2025-08-14 19:46:38 +0000

    teste2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -33,13 +33,47 @@ int main() {
     scanf("%f", &area1);
     printf("PIB (Bilhões de reais)\n");
     scanf("%f", &pib1);
-    printf("");
+    printf("Números de pontos turísticos\n");
+    scanf("%d", &pontos_turisticos1);
+
+    //Segunda carta
+    printf("Digite os dados da segunda carta:\n");
+    printf("Estado (A-H):");
+    scanf("%s", &estado2);
+    printf("Código da carta:\n");
+    scanf("%s", &codigo2);
+    printf("Nome da cidade:\n");
+    scanf("%s", &nome_cidade2);
+    printf("População:\n");
+    scanf("%d", &populacao2);
+    printf("Área em KM²:\n");
+    scanf("%f", &area2);
+    printf("PIB (Bilhões de reais)\n");
+    scanf("%f", &pib2);
+    printf("Números de pontos turísticos\n");
+    scanf("%d", &pontos_turisticos2);
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("\nCarta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nome_cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nome_cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
 
     return 0;
 }
