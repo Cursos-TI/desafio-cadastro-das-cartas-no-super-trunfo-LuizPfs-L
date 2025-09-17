9bb4bbb516c6ef060e7cce3e2713c3a2c6510486
LuizPFS <luizpfs09@gmail.com>
2025-09-17 18:25:35 +0000

    Desafio nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,12 +13,14 @@ int main() {
     char estado1[2],estado2[2];
     char codigo1[4],codigo2[4];
     char nome_cidade1[49],nome_cidade2[49];
-    int populacao1,populacao2;
+    unsigned long int populacao1,populacao2;
     float area1,area2;
     float pib1,pib2;
     int pontos_turisticos1,pontos_turisticos2;
     float densidade_populacional1,densidade_populacional2;
     float PIB_per_capita1, PIB_per_capita2;
+    float calcula_SuperPoder(unsigned long int populacao, float area, float pib, int pontos_turisticos, float densidade, float pib_per_capita);
+   
     // Cadastro das Cartas:
     //Primeira carta
     printf("Digite os dados da primeira carta:\n");
@@ -30,7 +32,7 @@ int main() {
     printf("Exemplo: Rio_de_Janeiro ou Belo-Horizonte\n");
     scanf("%s", nome_cidade1);
     printf("População:\n");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
     printf("Área em KM²:\n");
     scanf("%f", &area1);
     printf("PIB (bilhões de reais)\n");
@@ -48,7 +50,7 @@ int main() {
     printf("Exemplo: Rio_de_Janeiro ou Belo-Horizonte\n");
     scanf("%s", nome_cidade2);
     printf("População:\n");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
     printf("Área em KM²:\n");
     scanf("%f", &area2);
     printf("PIB (bilhões de reais)\n");
@@ -74,28 +76,46 @@ int main() {
     printf("Estado: %s\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nome_cidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
     printf("Densidade Populacional: %.2f\n", densidade_populacional1);
-    printf("PIB per Capita: %.2f\n", PIB_per_capita1);
+    printf("PIB per Capita: %.6f\n", PIB_per_capita1);
 
     
     printf("\nCarta 2:\n");
     printf("Estado: %s\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nome_cidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
     printf("Densidade Populacional: %.2f\n", densidade_populacional2);
-    printf("PIB per Capita: %.2f\n", PIB_per_capita2);
+    printf("PIB per Capita: %.6f\n", PIB_per_capita2);
 
+    //Desafio Nivel Mestre:Comparação
+    printf("\nComparação dos atributos (1 = Carta 1 vence, 0 = Carta 2 vence):\n");
+    printf("População: %d\n", populacao1 > populacao2 ? 1 : 0);
+    printf("Área: %d\n", area1 > area2 ? 1 : 0);
+    printf("PIB: %d\n", pib1 > pib2 ? 1 : 0);
+    printf("Pontos Turísticos: %d\n", pontos_turisticos1 > pontos_turisticos2 ? 1 : 0);
+    printf("Densidade Populacional: %d\n", densidade_populacional1 > densidade_populacional2 ? 1 : 0);
+    printf("PIB per Capita: %d\n", PIB_per_capita1 > PIB_per_capita2 ? 1 : 0);
+
+    // Super Poder
+    
+    float super_poder1 = calculaSuperPoder(populacao1, area1, pib1, pontos_turisticos1, densidade_populacional1, PIB_per_capita1);
+    float super_poder2 = calculaSuperPoder(populacao2, area2, pib2, pontos_turisticos2, densidade_populacional2, PIB_per_capita2);
+    printf("Super Poder: %d\n", super_poder1 > super_poder2 ? 1 : 0);
+    
+    
 
     printf("!DESAFIO NIVEL NOVATO CONCLUIDO!\n");
     printf("!DESAFIO NIVEL AVENTUREIRO CONCLUIDO!\n");
+    printf("!DESAFIO NIVEL MESTRE CONCLUIDO!\n");
+    printf("!TODOS OS DESAFIOS CONCLUIDOS!\n");
 
     return 0;
 }
